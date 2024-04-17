723b980c72aff7ec612b4951b9a32143ff471ae0
Zaryab Haider <93727250+ZaryabXProgrammer@users.noreply.github.com>
2024-04-18 01:10:18 +0500

    removed local host added render ip

--- src/RequestMethods.js
+++ src/RequestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3003/api/'
+const BASE_URL = 'https://e-commerce-store-api-backend.onrender.com/api/'
 
 // const TOKEN =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MmVjMjdhNWQ3YTc2YzNiODkzODY4OSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2OTc1NjMzMDEsImV4cCI6MTY5NzgyMjUwMX0.fl7JTvTY7YRUjniNDdvgxkubyJ_JLjUBpnyG5pAAebk";
